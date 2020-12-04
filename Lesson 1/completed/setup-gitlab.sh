0ba79a8221bebd0696c2f76705aadd22b52b07b8
Tim Urista <timothy.urista@gmail.com>
2020-12-04 08:21:29 -0800

    add inital install step for gitlab

--- Lesson 1/completed/setup-gitlab.sh
+++ Lesson 1/completed/setup-gitlab.sh
@@ -4,7 +4,9 @@ set -ex
 
 kubectl config current-context
 
+# see: https://docs.gitlab.com/charts/
+
 helm repo add gitlab https://charts.gitlab.io/
 helm repo update
-helm get values gitlab > gitlab.yaml
-helm upgrade gitlab gitlab/gitlab -f gitlab.yaml
+helm install my-gitlab gitlab/gitlab \
+    --set certmanager-issuer.email=me@example.com
